0d012cbdd8d48f280b131b9bb464ebe37e7de3c0
devdave0 <dbchung24@gmail.com>
2020-08-30 23:49:09 -0400

    imported Provider from react redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index'
+import { Provider } from 'react-redux'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );

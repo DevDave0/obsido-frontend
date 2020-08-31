e742117506875a84712da305130a89ed37fa312d
devdave0 <dbchung24@gmail.com>
2020-08-31 11:32:20 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import Chart from './components/Chart';
 import SignUp from './SignUp';
 import Login from './Login'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
+      <BrowserRouter>
+        <div className="App">
           <h1>PieChart</h1>
           <Login />
           <SignUp />
           <Chart />
-      </div>
 
+          <Route path="/" component={ Login } />
+        </div>
+      </BrowserRouter>
     );
   }
 }

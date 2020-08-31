2dfde13b27098e25cda755299b706748eee93fd7
devdave0 <dbchung24@gmail.com>
2020-08-31 00:01:12 -0400

    added practice actions

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { increment, decrement } from '../actions/index'
 
 const Chart = () => {
+    const counter = useSelector(state => state.counterReducer)
+    const isLogged = useSelector(state => state.loggedReducer)
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h1>My Chart</h1>
+            <h2>Counter {counter}</h2>
+            <button onClick={() => dispatch(increment())} >+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+
+            {isLogged ? <h3>Valuable information I shouldn't see</h3> : ""}
+
         </div>
     )
 }

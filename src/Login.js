e7eed93c87a7789e91c432de4898232eeb564b45
devdave0 <dbchung24@gmail.com>
2020-08-31 13:36:02 -0400

    finished login styling

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Grid, Header, Message, Segment, Icon } from 'semantic-ui-react'
 
 class Login extends Component {
 
@@ -30,15 +31,26 @@ class Login extends Component {
     render() {
         return(
             <div>
-                <h2>Login</h2>
-                <form onSubmit={(e)=> this.handleSubmit(e)}>
-                    <label>Username: </label>
-                    <input onChange={(e) => this.handleChange(e)} name='name' type='text'/>
-                    <label>Password: </label>
-                    <input onChange={(e) => this.handleChange(e)} name='password' type='password'/>
-                    <input type='submit'/>
-                </form>
-                < Link to='/sign_up'>Sign Up</Link>
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' color='teal' textAlign='center'>
+                            <Icon name='check circle' />
+                            Log-in to your account
+                        </Header>
+                        <form size='large' onSubmit={(e)=> this.handleSubmit(e)}>
+                            <Segment stacked>
+                            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' onChange={(e) => this.handleChange(e)} name='name' type='text' />
+                            <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' onChange={(e) => this.handleChange(e)} name='password' type='password'  />
+                            <br></br>
+                            <Form.Input type='submit'/>
+                            </Segment>
+                        </form>
+                        <Message>
+                            New to us? < Link to='/sign_up'>Sign Up</Link>
+                        </Message>
+
+                    </Grid.Column>
+                </Grid>
             </div>
         )
     }

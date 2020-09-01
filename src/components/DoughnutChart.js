50dbfb625b5ac140a1adbd89ce7cd44747b38624
devdave0 <dbchung24@gmail.com>
2020-09-01 16:38:47 -0400

    added chart event

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -12,34 +12,36 @@ const DoughnutChart = () => {
                 data: [35, 30, 15, 5, 5, 10],
                 backgroundColor: [
                     'rgba(255, 99, 132, 1',
-                    'rgba(70, 235, 70, 1',
-                    'rgba(54, 162, 235, 1',
                     'rgba(255, 159, 64, 1',
-                    'rgba(235, 235, 70, 1',
+                    'rgba(235, 235, 70, 1', 
+                    'rgba(70, 235, 70, 1',  
+                    'rgba(54, 162, 235, 1', 
                     'rgba(153, 102, 255, 1',
                 ]
             }
         ]
     }
 
-    const options = {
-        title: {
-            display: true, 
-            text: 'Doughnut Chart'
-        }
-    }
+    // const options = {
+    //     title: {
+    //         display: true, 
+    //         text: 'Doughnut Chart'
+    //     }
+    // }
 
     return (
         <div className='chart'>
             <Doughnut 
                 data={data}
+                onElementsClick={element => {console.log(element[0])}}
                 width={200}
                 height={500}
                 options={{
                     maintainAspectRatio: false,
                     animation: {
                         animateScale: true
-                    }
+                    },
+                    // onClick: (e, element) => {alert(e.target)}
                 }}
             />
         </div>

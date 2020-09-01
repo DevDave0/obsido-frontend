b9e0f5f1f3092ede32881d85e09a3b920e05cf2d
devdave0 <dbchung24@gmail.com>
2020-09-01 12:20:41 -0400

    added react hooks to form

--- src/components/CategoryForm.js
+++ src/components/CategoryForm.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
 import { addCategory } from '../actions/index'
 import {connect} from 'react-redux';
+import {CATEGORIES} from '../data';
 
-
-const CategoryForm = () => {
+const CategoryForm = (props) => {
 
     // take the state we get from the form submit, send it to the reducer with our action
     // within the reducer, we do a POST request to the backend(within the reducer) to create a category with our info within the body
     // once we make the category, we change the state of the piechart accordingly. 
+
+    const [category, setCategory] = useState('Misc')
+
+    const submit = (e) => {
+        e.preventDefault();
+        setCategory('Misc')
+        props.addCategory({category})
+    }
+
     return (
         <div>
             <h1>CategoryForm</h1>
@@ -17,14 +26,10 @@ const CategoryForm = () => {
                     Pick a Category
                 </label>
                     <select>
-                        <option>
-                            1
-                        </option>
+                        {
+                            CATEGORIES.map(category => <option key={category}>{category}</option>)
+                        }
                     </select>
-                <label>
-                    Add a category
-                </label>
-                <input placeholder="Add a category" type='text' />
                 <input placeholder="Description" type='text' />
                 <input type='submit' value='Add category' />
             </form>

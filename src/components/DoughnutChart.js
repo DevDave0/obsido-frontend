8b3fb0e3b49b0da1e5c8729713ff86b40e9fcc47
devdave0 <dbchung24@gmail.com>
2020-09-01 16:00:50 -0400

    working static donut chart

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Doughnut} from 'react-chartjs-2'
 
 
-class DoughnutChart extends Component {
-
-    render() {
-
-        return (
-            <div className='chart'>
-                <Doughnut />
-            </div>
-        )
+const DoughnutChart = () => {
+    
+    const data = {
+        labels: ['Stocks', 'Cryptos', 'Food', 'Bills', 'Shopping', 'Misc'],
+        datasets: [
+            {
+                label: 'Percentage',
+                data: [35, 30, 15, 5, 5, 10],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1',
+                    'rgba(70, 235, 70, 1',
+                    'rgba(54, 162, 235, 1',
+                    'rgba(255, 159, 64, 1',
+                    'rgba(235, 235, 70, 1',
+                    'rgba(153, 102, 255, 1',
+                ]
+            }
+        ]
     }
+
+    const options = {
+        title: {
+            display: true, 
+            text: 'Doughnut Chart'
+        }
+    }
+
+    return (
+        <div className='chart'>
+            <Doughnut 
+                data={data}
+                width={200}
+                height={500}
+                options={{
+                    maintainAspectRatio: false,
+                    animation: {
+                        animateScale: true
+                    }
+                }}
+            />
+        </div>
+    )
+
 }
 
 export default DoughnutChart;
